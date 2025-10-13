acbd58062fe0ac8161c86dcb3a0620135cc82c65
Jan Wierzchowski <wierzchowski.jan49@gmail.com>
2025-10-13 12:36:17 +0200

    settings

--- Assets/_Game/Code/Scripts/Managers/MainMenuManager.cs
+++ Assets/_Game/Code/Scripts/Managers/MainMenuManager.cs
@@ -6,9 +6,12 @@ namespace YNQ.JumpyJoe
     public class MainMenuManager : MonoBehaviour
     {
         [SerializeField] private MenuStrings _menuStrings;
+        [SerializeField] private InputReference _reference;
         [SerializeField] private Canvas _canvas;
         [SerializeField] private TextMeshProUGUI _bestSessionText;
         [SerializeField] private TextMeshProUGUI _bestGlobalText;
+        [SerializeField] private TMP_InputField _minDb;
+        [SerializeField] private TMP_InputField _maxDb;
         [SerializeField] private Animator _animator;
 
         private GameManager _gameManager;
@@ -19,6 +22,14 @@ namespace YNQ.JumpyJoe
 
             _bestSessionText.text = _menuStrings.bestSession.Construct(statsManager.GetBestDistanceSession(0));
             _bestGlobalText.text = _menuStrings.bestGlobal.Construct(statsManager.GetBestDistanceGlobal(0));
+
+            _minDb.text = _reference.minDbTreshold.ToString();
+            _maxDb.text = _reference.maxDbTreshold.ToString();
+            
+            _minDb.onSubmit.AddListener(v => _reference.minDbTreshold = float.Parse(v));
+            _minDb.onDeselect.AddListener(v => _reference.minDbTreshold = float.Parse(v));
+            _maxDb.onSubmit.AddListener(v => _reference.maxDbTreshold = float.Parse(v));
+            _maxDb.onDeselect.AddListener(v => _reference.maxDbTreshold = float.Parse(v));
             
             _canvas.gameObject.SetActive(true);
         }
